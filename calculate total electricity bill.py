3684174f1a71a2564f9888e93d4d049cd57406d4
Ragini Singh Thakur <raginisinghthakur039@gmail.com>
2026-04-02 12:56:53 +0530

    Python Code

--- calculate total electricity bill.py
+++ calculate total electricity bill.py
@@ -4,10 +4,10 @@ if unit<=50:
 elif unit<=200:
     bill=50*0.50+(unit-50)*0.75
 elif unit<=450:
-    bill=50*0.50+150*0.75+(unit-200)*1.20
+    bill=50*0.50+150*0.75+((unit-200)*1.20)
 else:
-    bill=50*0.50+150*0.75+250*1.20+(unit-450)*1.50
-    final_bill=bill*1.20
-    print("Final bill is",final_bill)
+    bill=50*0.50+150*0.75+250*1.20+((unit-450)*1.50)
+    bill=bill*1.20
+    print("Final bill is",bill)
     
     
